e1545af812d04e0457a0c813b5028d9336b3c2ab
Pedro Ferreira <pedrodsf21@gmail.com>
2025-03-29 17:16:30 +0000

    Update entity controller

--- src/entity/controllers.py
+++ src/entity/controllers.py
@@ -23,9 +23,9 @@ def delete_entity(user, entity):
     entity.delete()
 
 
-def get_entity_by_id(entity_id):
+def get_entity_by_id(user, entity_id):
     try:
-        entity = Entity.objects.get(id=entity_id)
+        entity = Entity.objects.get(id=entity_id, user=user)
         return entity
     except Entity.DoesNotExist:
         raise NotFound("Entity not found")
